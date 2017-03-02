8643ecdb340197b1704de1d03b5bdb92ffa135c3
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2017-03-02 00:36:24 -0500

    new project start

---
+++ build/Debug/GNU-Linux/hello.o.d
@@ -0,0 +1,3 @@
+build/Debug/GNU-Linux/hello.o: hello.cpp hello.h
+
+hello.h:
---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: gocmenta
+ *
+ * Created on March 2, 2017, 12:25 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include "hello.h"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+  hello();
+  
+  return 0;
+}
+
