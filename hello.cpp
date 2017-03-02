e39e46f1874b5eb0be8db2f3b26c51003a383810
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2017-03-02 01:00:51 -0500

    start a new project

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "hello.h"
+
+using namespace std;
+
+void hello(void){
+  cout << "Start of program" << endl;
+  cout << "Hello, World!" << endl;
+  cout << "End of program" << endl;
+}
---
+++ hello.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   hello.h
+ * Author: gocmenta
+ *
+ * Created on March 2, 2017, 12:28 AM
+ */
+
+#ifndef HELLO_H
+#define HELLO_H
+
+void hello(void);
+
+#endif /* HELLO_H */
+
